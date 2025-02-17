2a3d18baf7f1db5fbabf07127b4bbd65348487cc
akshaikrishnan <akshaik143@gmail.com>
2025-02-17 20:37:41 +0530

    config middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,8 +8,9 @@ export function middleware(request: NextRequest) {
     return NextResponse.next();
   }
   const sellerurl = process.env.NEXT_PUBLIC_SELLER_URL;
-  const loginUrl = new URL("/login", sellerurl);
+  const loginUrl = new URL("/auth/login", sellerurl);
   loginUrl.searchParams.set("from", request.nextUrl.pathname);
+  loginUrl.searchParams.set("userType", "Buyer");
 
   return NextResponse.redirect(loginUrl);
 }
