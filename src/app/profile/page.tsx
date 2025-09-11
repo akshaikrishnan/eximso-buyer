477cccb109f3d9e8b5c644e27a39331bec9a43fe
arjyouuu <arjunms4774@gmail.com>
2025-09-11 14:28:44 +0530

    EXI-55-Profile-navigation-fix-in-mobile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -168,13 +168,13 @@ export default function UserProfile() {
                     <label className="block text-sm font-medium leading-6 text-gray-900">
                       Photo
                     </label>
-                    <button
+                    {/* <button
                       type="button"
                       onClick={handleLogoChangeButtonClick} // Trigger file input click
                       className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
                     >
                       Change
-                    </button>
+                    </button> */}
                     <input
                       type="file"
                       accept="image/*"
