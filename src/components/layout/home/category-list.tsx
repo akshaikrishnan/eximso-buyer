d1e8623874bca783d5a2f5211775a4da3929049b
akshaikrishnan <akshaik143@gmail.com>
2025-09-13 19:18:09 +0530

    fixes

--- src/components/layout/home/category-list.tsx
+++ src/components/layout/home/category-list.tsx
@@ -40,9 +40,17 @@ const ListItem = ({
 };
 
 export default async function CategoryList() {
-  const categories = await api
-    .get(`${endpoints.categories}/top?limit=9`)
-    .then((res) => res.data);
+  // const categories = await api
+  //   .get(`${endpoints.categories}/top?limit=9`)
+  //   .then((res) => res.data);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/${endpoints.categories}/top?limit=9`,
+    {
+      cache: "force-cache",
+      next: { revalidate: 3600, tags: ["cache", "categories"] },
+    } // revalidate every hour
+  );
+  const categories = await res.json();
   return (
     <div className="space-y-2">
       {categories.map((item: any) => (
