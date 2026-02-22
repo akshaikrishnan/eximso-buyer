f99fa1c53461fdda0e97b598ebacc7a9149dafa2
akshaikrishnan <akshaik143@gmail.com>
2026-02-22 14:09:25 +0530

    feat: Implement user profile layout with dedicated navigation, add an unauthorized access page, and enable Next.js experimental auth interrupts.

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,7 +1,27 @@
 import ProfileNavbar from "@/components/profile/profile-navbar";
 import React from "react";
-
+import { unauthorized } from "next/navigation";
+import api from "@/lib/api/axios.interceptor";
+import { endpoints } from "@/lib/data/endpoints";
 export default function layout({ children }: { children: React.ReactNode }) {
+  type UserRecord = {
+    _id: string;
+    name: string;
+    email: string;
+    phone?: string;
+    gender?: "male" | "female" | "other" | "";
+    country?: string; // ISO code
+    address?: string;
+    logo?: string;
+  };
+
+  function getUser(): Promise<UserRecord> {
+    return api.get(endpoints.user).then((r) => r?.data?.result ?? {});
+  }
+  const user = getUser();
+  if (!user) {
+    unauthorized();
+  }
   return (
     <>
       <ProfileNavbar />
